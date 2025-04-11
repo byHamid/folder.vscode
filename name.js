32653fd4a137eac7fcacd42d978b6ca58559af6f
Hamid <hamisgen24@gmail.com>
2025-04-12 03:51:47 +0400

    Delete name.js

--- name.js
+++
@@ -1,8 +0,0 @@
-const users = [ 
-    { name: "Elvin", surname: "Huseynov" },
-    { name: "Ruslan", surname: "Mammadov" }
-  ];
-  
-  const fullNames = users.map(user => `${user.name} ${user.surname}`);
-  
-  console.log(fullNames);
